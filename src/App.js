aa338515fab19d5f8b6eaf68ee9067ec6dcbb866
prazman <olivierpraseuth@gmail.com>
2018-05-15 11:53:34 +0200

    added unit selection, started 2 pages

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function load(method, url, data, successcallback, errorcallback) {
 
 function UnitSelector(props){
   return (
-      <select id="unitInput" value={props.unit}>
+      <select onChange={props.onSelectChange} id="unitInput" >
         <option value="metric" >Metric </option>
         <option value="imperial" > Imperial </option>
       </select>
@@ -54,15 +54,27 @@ function UnitSelector(props){
 function WeatherPage(props){
 
   if(props.weather){
+    let temp;
+    let wind_speed;
+    if(props.unit == "metric"){
+      temp = props.weather.main.temp + " °C"
+      wind_speed = props.weather.wind.speed + " meter/sec";
+    }
+    else{
+      temp = props.weather.main.temp + " °F"
+       wind_speed = props.weather.wind.speed + " miles/hour";
+    }
+
     return (
       <div id="weatherPage">
         <div> Current Temperature:  
-        <span>{props.weather.main.temp - 273.15}</span></div>
+        <span>{temp}</span></div>
         <div> Current Weather :
         <span>{props.weather.weather[0].description}</span>
         </div>
-        <div> Current Wind Speed </div>
-        <span>{props.weather.wind.speed}</span>
+        <div> Current Wind Speed :
+        <span>{wind_speed} </span>
+        </div>
       </div>
           )
   }
@@ -71,8 +83,8 @@ function WeatherPage(props){
 function SunrisePage(props){
 
   if(props.weather){
-    const sunrise = new Date(props.weather.sys.sunrise*1000)
-    const sunset = new Date(props.weather.sys.sunset*1000)
+    const sunrise = new Date(props.weather.sys.sunrise*1000).toLocaleTimeString()
+    const sunset = new Date(props.weather.sys.sunset*1000).toLocaleTimeString()
     return (
       <div id="sunrisePage">
         <div> Sunrise at :  
@@ -89,9 +101,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {time: new Date(),
-                  unit:"metric"};
+                  unit:"metric",
+                  page:"weather"};
   }
   render() {
+    let component
+    if(this.state.page="weather"){
+      component = <WeatherPage unit={this.state.unit} weather={this.state.weather}></WeatherPage>
+    }
+    else{
+      component = <SunrisePage weather={this.state.weather}></SunrisePage>
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -99,24 +119,34 @@ class App extends Component {
           <h1 className="App-title">ShipUp Front End Challenge</h1>
         </header>
 
-        <UnitSelector unit={this.state.unit}></UnitSelector>
+        <UnitSelector onSelectChange={this.changeButtonState.bind(this)} unit={this.state.unit}></UnitSelector>
+        <h2> {this.state.title} </h2>
         <div>Current Time: 
         <span>{this.state.time.toString()}</span>
         </div>
-        <WeatherPage weather={this.state.weather}></WeatherPage>
-        <SunrisePage weather={this.state.weather}></SunrisePage>
+        {component}
+        
       </div>
     );
   }
   componentDidMount(){
-    let self = this
-    load("GET",openWeatherUrl + "?q=Paris&APPID="+openWeatherKey,null,function(response){
-      self.setState({weather: JSON.parse(response.response),loaded:true}) 
+    this.fetchData(this.state.unit)
+  }
+  changeButtonState(event) {
+    console.log('set to ' + event.target.value)
+    this.setState({unit: event.target.value})
+    this.fetchData(event.target.value)
+
+}
+fetchData(unit){
+  let self = this
+    load("GET",openWeatherUrl + "?q=Paris&units="+ unit+"&APPID="+openWeatherKey,null,function(response){
+      self.setState({weather: JSON.parse(response.response)}) 
       console.log(self.state)
     },function(error){
       console.log(error);
     })
-  }
+}
   
 }
 
